71890d2d3eeae9b12e8fea92a6527830a7a175b7
ZhangFuYu <zfykzgc@163.com>
2017-03-24 23:02:10 +0800

    移植修改第一天

---
+++ u-boot-2013.04-rc1-day1/include/config.mk
@@ -0,0 +1,6 @@
+ARCH   = arm
+CPU    = arm1176
+BOARD  = smdk6400
+VENDOR = samsung
+SOC    = s3c64xx
+CONFIG_NAND_U_BOOT = y
---
+++ u-boot-2013.04-rc1-day1/include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Mar 24 2017"
+#define U_BOOT_TIME "22:30:01"
